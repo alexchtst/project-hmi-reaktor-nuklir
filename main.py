9c7d28dacb3d8ec8f34651660bd17beec6279d99
alexchtst-01 <acinatra@gmail.com>
2025-11-29 21:56:35 +0700

    feat:
    [DONE]: implement start-calculation in ui
    [DONE]: change the asset and data path, cases, event-cases as dynamic configuration [presist]

--- main.py
+++ main.py
@@ -9,18 +9,34 @@ from ui.scenes.ScenarioScene import ScenarioScreenScene
 from ui.scenes.LoadFlowActifity import LoadflowActifityScreenScene
 from ui.scenes.DynamicSimulation import DynamicActifityScreenScene
 
+
 class MainApp(QStackedWidget):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("HMI Reaktor Nuklir")
-        self.setWindowIcon(QIcon(r"C:\Users\MSI\code-base\project-hmi-reaktor-nuklir\project-hmi-reaktor-nuklir\asset\logo-ugm.jpg"))
-        
+        self.setWindowIcon(QIcon(
+            r"C:\Users\MSI\code-base\project-hmi-reaktor-nuklir\project-hmi-reaktor-nuklir\asset\logo-ugm.jpg"))
+
         # variables and options
         self.pltnssytemprojetcname = None
         self.digsilent_path = None
-        
+
         self.cases = []
         self.eventcases = {}
+
+        # new data sturcture
+        self.user_data_dspf = {
+            "39 Bus New England System": {
+                "dspf_path": None,
+                "cases": [],
+                "event_cases": None,
+            },
+            "Nine-bus System": {
+                "dspf_path": None,
+                "cases": [],
+                "event_cases": None,
+            },
+        }
 
         # Buat scene scene
         self.splash = CoverScreenScene()
@@ -30,27 +46,27 @@ class MainApp(QStackedWidget):
         self.loadflow = LoadflowActifityScreenScene()
         self.dynamic = DynamicActifityScreenScene()
 
-        self.addWidget(self.splash) # 0
-        self.addWidget(self.howto) # 1
-        self.addWidget(self.pltn) # 2
-        self.addWidget(self.scenario) # 3
-        self.addWidget(self.loadflow) # 4
-        self.addWidget(self.dynamic) # 5
+        self.addWidget(self.splash)  # 0
+        self.addWidget(self.howto)  # 1
+        self.addWidget(self.pltn)  # 2
+        self.addWidget(self.scenario)  # 3
+        self.addWidget(self.loadflow)  # 4
+        self.addWidget(self.dynamic)  # 5
 
         self.splash.finished.connect(self.show_howto)
         self.howto.nextpltn.connect(self.show_pltn)
         self.pltn.nextscenario.connect(self.show_scenario)
-        
+
         self.scenario.backtopltn.connect(self.show_pltn)
         self.scenario.toloadflow.connect(self.show_loadflowactifity)
         self.scenario.todynamic.connect(self.show_dynamicctifity)
-        
+
         self.dynamic.backcenario.connect(self.show_scenario)
         self.dynamic.backpltn.connect(self.show_pltn)
-        
+
         self.loadflow.backcenario.connect(self.show_scenario)
         self.loadflow.backpltn.connect(self.show_pltn)
-        
+
         self.pltn.pltnsystemsignal.connect(self.on_listen_pltn_system)
         self.pltn.digsilentpathsignal.connect(self.on_listen_digsilent_path)
         self.pltn.casessignal.connect(self.on_pltn_connect_signal)
@@ -58,44 +74,49 @@ class MainApp(QStackedWidget):
 
         self.setMinimumSize(1020, 680)
         self.setCurrentIndex(0)
-    
+
     def on_pltn_connect_signal(self, value):
         self.cases = value
-        
+
     def on_pltn_connect_caseseventsignal(self, value):
         self.eventcases = value
-    
+
     def on_listen_pltn_system(self, value):
         self.pltnssytemprojetcname = value
-    
+
     def on_listen_digsilent_path(self, value):
         self.digsilent_path = value
+        if self.pltnssytemprojetcname in list(self.user_data_dspf.keys()):
+            self.user_data_dspf[self.pltnssytemprojetcname]['dspf_path'] = value
+            self.user_data_dspf[self.pltnssytemprojetcname]['event_cases'] = self.eventcases
+            self.user_data_dspf[self.pltnssytemprojetcname]['cases'] = self.cases
 
     def show_howto(self):
         self.setCurrentIndex(1)
 
     def show_pltn(self):
         self.setCurrentIndex(2)
-        
+
     def show_scenario(self):
         self.setCurrentIndex(3)
-        self.scenario.digsilent_path = self.digsilent_path
+        self.scenario.digsilent_path = self.user_data_dspf[self.pltnssytemprojetcname]['dspf_path']
         self.scenario.proj_name = self.pltnssytemprojetcname
-    
+
     def show_loadflowactifity(self):
         self.setCurrentIndex(4)
-        
-        self.loadflow.ds_pf_path_signal.emit(self.digsilent_path)
+
+        self.loadflow.ds_pf_path_signal.emit(self.user_data_dspf[self.pltnssytemprojetcname]['dspf_path'])
         self.loadflow.project_name_signal.emit(self.pltnssytemprojetcname)
-        self.loadflow.caseses_signal.emit(self.cases)
-    
+        self.loadflow.caseses_signal.emit(self.user_data_dspf[self.pltnssytemprojetcname]['cases'])
+
     def show_dynamicctifity(self):
         self.setCurrentIndex(5)
-        
-        self.dynamic.ds_pf_path_signal.emit(self.digsilent_path)
+
+        self.dynamic.ds_pf_path_signal.emit(self.user_data_dspf[self.pltnssytemprojetcname]['dspf_path'])
         self.dynamic.project_name_signal.emit(self.pltnssytemprojetcname)
-        self.dynamic.caseses_signal.emit(self.cases)
-        self.dynamic.casesesevent_signal.emit(self.eventcases)
+        self.dynamic.caseses_signal.emit(self.user_data_dspf[self.pltnssytemprojetcname]['cases'])
+        self.dynamic.casesesevent_signal.emit(self.user_data_dspf[self.pltnssytemprojetcname]['event_cases'])
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
